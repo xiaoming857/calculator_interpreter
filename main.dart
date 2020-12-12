4c91af4e4fd15d95b5bb083ede608702638e6973
Arvin <gnimoaix857@gmail.com>
2020-12-13 02:09:00 +0700

    Update main

--- main.dart
+++ main.dart
@@ -6,17 +6,17 @@ import 'src/parser.dart';
 
 void main() {
   print('''
-    +============================+
-    |Supported Operations:       |
-    +----------------------------+
-    |- Addition & Subtraction    |
-    |- Multiplication & Division |
-    |- Exponential               |
-    +============================+
+    +======================================+
+    |Supported Operations:                 |
+    +--------------------------------------+
+    |- Addition & Subtraction              |
+    |- Multiplication & Division & Modulus |
+    |- Exponential                         |
+    +======================================+
   ''');
   stdout.write('Enter an expression: ');
   String expression = stdin.readLineSync(encoding: utf8);
-  List<Map<TOKEN_TYPE, String>> tokens = Lexer.getTokens(expression);
+  List<List<dynamic>> tokens = Lexer.getTokens(expression);
   print('TOKENS: $tokens\n');
-  print('RESULT: ${Parser().parse(tokens)}');
+  print('RESULT: ${Parser().parse(tokens).toString()}');
 }
