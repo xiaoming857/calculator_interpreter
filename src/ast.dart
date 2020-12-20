50b871c833d50a8638b104f82aa62fd9c6561f83
Arvin <gnimoaix857@gmail.com>
2020-12-20 20:20:32 +0700

    Added Assignment and Identifier

--- src/ast.dart
+++ src/ast.dart
@@ -8,61 +8,96 @@ class Node {
 }
 
 
-class Expression extends Node {
-  Operator opr;
-  Node lValue;
-  Node rValue;
-
-  Expression(this.opr, this.lValue, this.rValue);
+class Statement extends Node {
   @override
   String toString() {
-    return 'Expr(${this.lValue} ${this.opr} ${this.rValue})';
+    return 'Statement';
+  }
+}
+
+
+class Expression extends Node {
+  @override
+  String toString() {
+    return 'Expression()';
+  }
+}
+
+
+class Assignment extends Statement {
+  Identifier _identifier;
+  Node _value;
+
+  Assignment(this._identifier, this._value);
+
+  @override
+  String toString() {
+    return 'Assignemnt(${this._identifier} = ${this._value})';
   }
 }
 
 
 class BinaryOperation extends Expression {
-  BinaryOperation(Operator opr, Node lValue, Node rValue) : super(opr, lValue, rValue);
+  Operator _operator;
+  Node _lOperand;
+  Node _rOperand;
+
+  BinaryOperation(this._operator, this._lOperand, this._rOperand);
 
   @override
   String toString() {
-    return 'BinOp(${super.lValue} ${super.opr} ${super.rValue})';
+    return 'BinOp(${this._lOperand} ${this._operator} ${this._rOperand})';
   }
 }
 
 
-class UnaryOperation extends BinaryOperation {
-  UnaryOperation(Operator opr, rValue) : super (opr, null, rValue);
+class UnaryOperation extends Expression {
+  Operator _operator;
+  Node _operand;
+
+  UnaryOperation(this._operator, this._operand);
 
   @override
   String toString() {
-    return 'UnOp(${super.opr} ${super.rValue})';
+    return 'UnOp(${this._operator} ${this._operand})';
   }
 }
 
 
 class Operator extends Node {
-  TOKEN_TYPE token;
-  String symbol;
+  TOKEN_TYPE _token;
+  String _symbol;
 
-  Operator(this.token, this.symbol);
+  Operator(this._token, this._symbol);
 
   @override
   String toString() {
-    return 'Operator(${this.symbol})';
+    return 'Operator(${this._symbol})';
   }
 }
 
 
 class Number extends Node {
-  TOKEN_TYPE token = TOKEN_TYPE.NUMBER;
-  double value;
+  TOKEN_TYPE _token = TOKEN_TYPE.NUMBER;
+  double _value;
 
-  Number(this.value);
+  Number(this._value);
 
   @override
   String toString() {
-    return 'Number(${this.value})';
+    return 'Number(${this._value})';
+  }
+}
+
+
+class Identifier extends Node {
+  String _identifier;
+
+  Identifier(this._identifier);
+
+  @override
+  String toString() {
+    return 'Identifier(${this._identifier})';
   }
 }
 
